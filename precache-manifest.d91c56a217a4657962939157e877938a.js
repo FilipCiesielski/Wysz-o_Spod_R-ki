3e9e59fb79e3ad061a109181b0c2a3e7672f2e4e
FilipCiesielski <filip.carpenter@gmail.com>
2019-11-05 22:23:55 +0100

    Updates

---
+++ precache-manifest.d91c56a217a4657962939157e877938a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f740a9f948ec9132743b0dec8e0347a5",
+    "url": "/Wyszlo_Spod_Reki/index.html"
+  },
+  {
+    "revision": "ff68a95404a9a00b03ca",
+    "url": "/Wyszlo_Spod_Reki/static/css/main.22156e48.chunk.css"
+  },
+  {
+    "revision": "382016b0c31537835419",
+    "url": "/Wyszlo_Spod_Reki/static/js/2.5878fc06.chunk.js"
+  },
+  {
+    "revision": "ff68a95404a9a00b03ca",
+    "url": "/Wyszlo_Spod_Reki/static/js/main.7082123c.chunk.js"
+  },
+  {
+    "revision": "77fa5d1146720e34dd2d",
+    "url": "/Wyszlo_Spod_Reki/static/js/runtime-main.d61571f4.js"
+  },
+  {
+    "revision": "88dc189f551de2dc46411bb9d4760f19",
+    "url": "/Wyszlo_Spod_Reki/static/media/2618666.88dc189f.jpg"
+  },
+  {
+    "revision": "308d74593961c63533693d9c07e95412",
+    "url": "/Wyszlo_Spod_Reki/static/media/2878411.308d7459.jpg"
+  },
+  {
+    "revision": "e8c0da3ca5bc2c557b37437c6f1cdd3e",
+    "url": "/Wyszlo_Spod_Reki/static/media/634100-POB6T4-481.e8c0da3c.jpg"
+  },
+  {
+    "revision": "138360561fc5e6b28f76d53d3e2e99ac",
+    "url": "/Wyszlo_Spod_Reki/static/media/KasiaPopiołekColor.13836056.png"
+  },
+  {
+    "revision": "75e77acedba7fd898e9614542eae02ae",
+    "url": "/Wyszlo_Spod_Reki/static/media/Wyszło Spod Ręki.75e77ace.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Wyszlo_Spod_Reki/precache-manifest.d91c56a217a4657962939157e877938a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Wyszlo_Spod_Reki/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
